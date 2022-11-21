7e92cc13dff74dbf29d09766f3e87f0c5478e662
CleberCampos2011 <clebercampos2011@hotmail.com>
2022-11-20 21:10:28 -0300

    Add project files.

---
+++ Iniciante5_SalarioReajuste/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iniciante5_SalarioReajuste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\n*******************************************************************");
+            Console.WriteLine(">>>>>>>>>>  APLICAÇÃO PARA CALCULO DE REAJUSTE SALARIAL  <<<<<<<<<<");
+            Console.WriteLine("*******************************************************************");
+
+            int qtdFuncionarios = 0;
+            double salario = 0;
+            int i = 0;
+
+            Console.Write("\n\n\n\nDigite o número de funcionários que deseja calcular: ");
+            qtdFuncionarios = Convert.ToInt32(Console.ReadLine());
+
+            while (qtdFuncionarios <= 10)
+            {
+                i = i++;
+                Console.Write("Digite o salário do funcionário " + i + ": ");
+                salario = Convert.ToDouble(Console.ReadLine());
+            }
+
+
+
+
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("\n\n\n*******************************************************************");
+            Console.WriteLine(">>>>>>>>>>       APLICAÇÃO FINALIZADA COM SUCESSO        <<<<<<<<<<");
+            Console.WriteLine("*******************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
